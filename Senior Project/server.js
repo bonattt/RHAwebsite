63fef36e15a81561eeab7f5d76b97112e887f372
bonattt <bonattt@rose-hulman.edu>
2016-10-26 10:55:11 -0400

    added node.js modules and server

---
+++ Senior Project/server.js
@@ -0,0 +1,52 @@
+
+port = 8000;
+
+http = require('http');
+fs = require('fs');
+url = require('url');
+express = require('express');
+app = express();
+
+app.use(express.static('resources'));
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/html/RHAhome.html');
+});
+
+app.get('/proposals', function (req, res) {
+	res.sendFile(__dirname + '/html/proposals.html');
+});
+
+app.get('/sign-ups', function (req, res) {
+	res.sendFile(__dirname + '/html/sign-ups.html');
+});
+
+app.get('/pastEvents', function (req, res) {
+	res.sendFile(__dirname + '/html/pastEvents.html');
+});
+
+app.get('/subwayCam', function (req, res) {
+	res.sendFile(__dirname + '/html/subwayCam.html');
+});
+
+app.get('/floorMoney', function (req, res) {
+	res.sendFile(__dirname + '/html/floorMoney.html');
+});
+
+app.get('/officers', function (req, res) {
+	res.sendFile(__dirname + '/html/officers.html');
+});
+
+app.get('/committees', function (req, res) {
+	res.sendFile(__dirname + '/html/committees.html');
+});
+
+var server = app.listen(port, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log("Example app listening at http://%s:%s", host, port)
+});
+
+
+var address = 'http://127.0.0.1:' + port + '/';
+console.log('Server running at ' + address);
