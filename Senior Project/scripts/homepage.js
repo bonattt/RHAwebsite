0964d98a170a8790434cf08611785c0c82758767
seanmmcpherson <mcphersm@rose-hulman.edu>
2016-10-25 23:50:49 -0400

    RHAhome edit works, button visuals fixed
    yay

--- Senior Project/scripts/homepage.js
+++ Senior Project/scripts/homepage.js
@@ -1,12 +1,15 @@
 (function() {
 	var isAdmin = true;
+    var hasListener = false;
 
 	if (isAdmin) {
         var adminValues = document.getElementsByClassName("edit");
         for (var i = 0; i < adminValues.length; i++) {
             var editImage = document.createElement("img");
             editImage.setAttribute("src", "../images/edit.png");
-            adminValues[i].appendChild(editImage);
+            editImage.style.cssText = "float: right;";
+            // adminValues[i].appendChild(editImage);
+            adminValues[i].insertBefore(editImage, adminValues[i].firstChild);
             editImage.addEventListener("click", function (e) {
                 showModal(e);
             }, false);
@@ -17,6 +20,8 @@
     function showModal(editImage) {
     	var modal = document.getElementById('myModal');
         var span = document.getElementsByClassName("close")[0];
+        var img = editImage.srcElement.innerHTML;
+        var div = editImage.srcElement.parentElement;
 
         var newStuff = "Header: ";
         var newStuffDesc = "Description: ";
@@ -24,12 +29,12 @@
         var newStuffInput = document.createElement("textarea");
         newStuffInput.setAttribute("rows", "1");
         newStuffInput.setAttribute("cols", "20");
-        newStuffInput.innerHTML = editImage.srcElement.parentElement.innerHTML.split("<")[0];
+        newStuffInput.innerHTML = div.querySelectorAll(":nth-child(2)")[0].innerHTML;
 
         var descInput = document.createElement("textarea");
         descInput.setAttribute("rows", "4");
         descInput.setAttribute("cols", "20");
-        descInput.innerHTML = editImage.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(2)")[0].innerHTML;
+        descInput.innerHTML = div.querySelectorAll(":nth-child(3)")[0].innerHTML;
 
         var whatsnewNode = document.getElementById("whatsnewInput");
         var descNode = document.getElementById("descInput");
@@ -55,12 +60,24 @@
             }
         }
 
+
         var submit = document.getElementById("submit");
-        submit.addEventListener("click", )
+        submit.style.cssText = "height: 10%; width: 25%; font-size: 14px; float: right; color: black; margin-right: 40%;";
+        new_submit=submit.cloneNode(true);
+        new_submit.addEventListener("click", function() {
+            submitChanges(newStuffInput, descInput)
+        }, false);
+        submit.parentNode.replaceChild(new_submit, submit);
+
 
         
         function submitChanges(header, description) {
-            
+            div.querySelectorAll(":nth-child(2)")[0].innerHTML = header.value;
+            div.querySelectorAll(":nth-child(3)")[0].innerHTML = description.value;
+
+            modal.style.display = "none";
+            whatsnewNode.removeChild(whatsnewNode.firstChild);
+            descNode.removeChild(descNode.firstChild);
         }
     }
 
