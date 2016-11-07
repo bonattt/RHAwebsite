bc17b5178a6c87a096f8d4d654031b6a522bbdb7
cookmn <cookmn@cookmn-w530g2.rose-hulman.edu>
2016-11-06 21:41:01 -0500

    Putting works!!!

--- Senior Project/resources/scripts/signUps.js
+++ Senior Project/resources/scripts/signUps.js
@@ -1,30 +1,11 @@
+
+var eventsMap = new Object();
 function displayPastEvents() {
-    // var proposal = [{
-    //     name: "King's Island",
-    //     cost_to_attendee: 10.50,
-    //     event_date: "2016-11-10",
-    //     event_signup_open: "2016-11-01",
-    //     event_signup_close: "2016-11-04",
-    //     image_path: "../images/kingsIsland.jpg",
-    //     description: "newFakeEvent",
-    //     proposer_id: 44,
-    //     week_proposed: 5,
-    //     quarter_proposed: 1,
-    //     money_requested: 750,
-    //     approved: true
-    // }];
-
-
     var xhr = getEvents();
     xhr.send();
     setTimeout(function () { actuallyDoShit(xhr.responseText) }, 300);
-    // console.log("This is the xhr object: ");
-    // console.log(xhr);
-    // console.log("This SHOULD be its responseText: ");
-    // console.log(xhr.onload());
 
     function actuallyDoShit(proposal) {
-        // console.log(JSON.parse(proposal));
         proposal = JSON.parse(proposal);
 
         for (var i = 0; i < proposal.length; i++) {
@@ -47,30 +28,24 @@ function displayPastEvents() {
         console.log(url);
         function createCORSRequest(method, url) {
             var xhr = new XMLHttpRequest();
+            console.log("xhr is: ");
+            console.log(xhr);
             if ("withCredentials" in xhr) {
-
                 // Check if the XMLHttpRequest object has a "withCredentials" property.
                 // "withCredentials" only exists on XMLHTTPRequest2 objects.
                 xhr.open(method, url, true);
-
             } else if (typeof XDomainRequest != "undefined") {
-
                 // Otherwise, check if XDomainRequest.
                 // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
                 xhr = new XDomainRequest();
                 xhr.open(method, url);
-
             } else {
-
                 // Otherwise, CORS is not supported by the browser.
                 xhr = null;
-
             }
             return xhr;
         }
-
         var xhr = createCORSRequest('GET', url);
-        // console.log(xhr);
         if (!xhr) {
             throw new Error('CORS not supported');
         }
@@ -85,36 +60,48 @@ function displayPastEvents() {
             console.log("There was an error");
         }
         // xhr.send();
-        // console.log(xhr);
         return xhr;
 
     }
 
 }
 
-function displaySignUps() {
-    // var proposal = [{
-    //     name: "King's Island",
-    //     cost_to_attendee: 10.50,
-    //     event_date: "2016-11-10",
-    //     event_signup_open: "2016-11-01",
-    //     event_signup_close: "2016-11-04",
-    //     image_path: "../images/kingsIsland.jpg",
-    //     description: "newFakeEvent",
-    //     proposer_id: 44,
-    //     week_proposed: 5,
-    //     quarter_proposed: 1,
-    //     money_requested: 750,
-    //     approved: true
-    // }];
+function saveEvent() {
+    var url = apiURL + 'api/v1/events/1';
+    function createCORSRequest(method, url) {
+        var xhr = new XMLHttpRequest();
+        console.log("xhr is: ");
+        console.log(xhr);
+        xhr.open(method, url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        return xhr;
+    }
+    var xhr = createCORSRequest('PUT', url);
+    if (!xhr) {
+        throw new Error('CORS not supported');
+    }
 
+    xhr.onload = function () {
+        var responseText = xhr.responseText;
+        console.log("Response text: " + responseText);
+        // return responseText;
+    }
+
+    xhr.onerror = function () {
+        console.log("There was an error");
+    }
+    xhr.send(JSON.stringify({ proposal_name: "It Works!!!" }));
+    return xhr;
+
+}
+
+function displaySignUps() {
     var xhr = getEvents();
     xhr.send();
     setTimeout(function () { actuallyDoShit(xhr.responseText) }, 300);
 
     function actuallyDoShit(proposal) {
         proposal = JSON.parse(proposal);
-
         for (var i = 0; i < proposal.length; i++) {
             var html = "<div class='eventTile'><p class='signUpText edit'>" + proposal[i].proposal_name + " - " + proposal[i].cost_to_attendee + "</p>";
             html += "<img class='signUpImage' src =" + proposal[i].image_path + "></img>";
@@ -123,10 +110,12 @@ function displaySignUps() {
             html += "<a id='myBtn' class='viewListLink'> View List </a>";
             html += "<div class='moreInformation'>" + proposal[i].description + " Sign-ups for this event will close on " + proposal[i].event_signup_close + ".</div>";
             html += "</div>";
+            eventsMap[proposal[i].proposal_name] = proposal[i].proposal_id;
 
             var tileArea = document.getElementsByClassName("eventTileArea")[0];
             tileArea.innerHTML += html;
         }
+        console.log(eventsMap);
         var isAdmin = true;
         if (isAdmin) {
             displayEditingPens();
@@ -173,7 +162,6 @@ function displaySignUps() {
         }
 
         var xhr = createCORSRequest('GET', url);
-        // console.log(xhr);
         if (!xhr) {
             throw new Error('CORS not supported');
         }
@@ -188,90 +176,91 @@ function displaySignUps() {
             console.log("There was an error");
         }
         // xhr.send();
-        // console.log(xhr);
         return xhr;
 
     }
 
 }
 
-    function showEditModal(editImage) {
-        var modal = document.getElementById('editModal');
-        var span = document.getElementsByClassName("closeEdit")[0];
+function showEditModal(editImage) {
+    console.log(editImage.srcElement);
+    var modal = document.getElementById('editModal');
+    var span = document.getElementsByClassName("closeEdit")[0];
 
-        var title = editImage.srcElement.parentElement.innerHTML.split(" - ");
-        var name = "Event name: ";
-        var price = "Price: ";
-        var image = "Image: ";
-        var description = "Description: ";
-        var signUpCloseDate = "Sign-up close date: ";
+    var title = editImage.srcElement.parentElement.innerHTML.split(" - ");
+    var name = "Event name: ";
+    var price = "Price: ";
+    var image = "Image: ";
+    var description = "Description: ";
+    var signUpCloseDate = "Sign-up close date: ";
 
-        var nameInput = document.createElement("textarea");
-        nameInput.setAttribute("rows", "1");
-        nameInput.setAttribute("cols", "30");
-        nameInput.innerHTML = title[0];
+    var nameInput = document.createElement("textarea");
+    nameInput.setAttribute("rows", "1");
+    nameInput.setAttribute("cols", "30");
+    nameInput.innerHTML = title[0];
 
-        var priceInput = document.createElement("textarea");
-        priceInput.setAttribute("rows", "1");
-        priceInput.setAttribute("cols", "30");
-        priceInput.innerHTML = title[1].split("<")[0];
+    var priceInput = document.createElement("textarea");
+    priceInput.setAttribute("rows", "1");
+    priceInput.setAttribute("cols", "30");
+    priceInput.innerHTML = title[1].split("<")[0];
 
-        var descriptionInput = document.createElement("textarea");
-        descriptionInput.setAttribute("rows", "4");
-        descriptionInput.setAttribute("cols", "30");
-        descriptionInput.innerHTML = editImage.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(6)")[0].innerHTML.split(" Sign-ups for this event will close on ")[0];
+    var descriptionInput = document.createElement("textarea");
+    descriptionInput.setAttribute("rows", "4");
+    descriptionInput.setAttribute("cols", "30");
+    descriptionInput.innerHTML = editImage.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(6)")[0].innerHTML.split(" Sign-ups for this event will close on ")[0];
 
-        var signUpCloseDateInput = document.createElement("textarea");
-        signUpCloseDateInput.setAttribute("rows", "1");
-        signUpCloseDateInput.setAttribute("cols", "30");
-        signUpCloseDateInput.innerHTML = editImage.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(6)")[0].innerHTML.split(" Sign-ups for this event will close on ")[1].split(".")[0];
-        console.log(editImage.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(2)")[0].currentSrc.split("images/")[1]);
+    var signUpCloseDateInput = document.createElement("textarea");
+    signUpCloseDateInput.setAttribute("rows", "1");
+    signUpCloseDateInput.setAttribute("cols", "30");
+    signUpCloseDateInput.innerHTML = editImage.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(6)")[0].innerHTML.split(" Sign-ups for this event will close on ")[1].split(".")[0];
+    // console.log(editImage.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(2)")[0].currentSrc.split("images/")[1]);
 
-        var imageInput = document.createElement("textarea");
-        imageInput.setAttribute("rows", "1");
-        imageInput.setAttribute("cols", "30");
-        imageInput.innerHTML = editImage.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(2)")[0].currentSrc.split("images/")[1];
+    var imageInput = document.createElement("textarea");
+    imageInput.setAttribute("rows", "1");
+    imageInput.setAttribute("cols", "30");
+    imageInput.innerHTML = editImage.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(2)")[0].currentSrc.split("images/")[1];
 
-        var nameNode = document.getElementById("nameInput");
-        var priceNode = document.getElementById("priceInput");
-        var imageNode = document.getElementById("imageInput");
-        var descriptionNode = document.getElementById("descriptionInput");
-        var signUpCloseDateNode = document.getElementById("signUpCloseDateInput");
-
-
-        document.getElementById("name").innerHTML = name;
-        nameNode.appendChild(nameInput);
-        document.getElementById("price").innerHTML = price;
-        priceNode.appendChild(priceInput);
-        document.getElementById("image").innerHTML = image;
-        imageNode.appendChild(imageInput);
-        document.getElementById("description").innerHTML = description;
-        descriptionNode.appendChild(descriptionInput);
-        document.getElementById("signUpCloseDate").innerHTML = signUpCloseDate;
-        signUpCloseDateNode.appendChild(signUpCloseDateInput);
+    var nameNode = document.getElementById("nameInput");
+    var priceNode = document.getElementById("priceInput");
+    var imageNode = document.getElementById("imageInput");
+    var descriptionNode = document.getElementById("descriptionInput");
+    var signUpCloseDateNode = document.getElementById("signUpCloseDateInput");
 
 
-        modal.style.display = "block";
-        span.onclick = function () {
+    document.getElementById("name").innerHTML = name;
+    nameNode.appendChild(nameInput);
+    document.getElementById("price").innerHTML = price;
+    priceNode.appendChild(priceInput);
+    document.getElementById("image").innerHTML = image;
+    imageNode.appendChild(imageInput);
+    document.getElementById("description").innerHTML = description;
+    descriptionNode.appendChild(descriptionInput);
+    document.getElementById("signUpCloseDate").innerHTML = signUpCloseDate;
+    signUpCloseDateNode.appendChild(signUpCloseDateInput);
+
+
+    modal.style.display = "block";
+    span.onclick = function () {
+        modal.style.display = "none";
+        nameNode.removeChild(nameNode.firstChild);
+        priceNode.removeChild(priceNode.firstChild);
+        imageNode.removeChild(imageNode.firstChild);
+        descriptionNode.removeChild(descriptionNode.firstChild);
+        signUpCloseDateNode.removeChild(signUpCloseDateNode.firstChild);
+        saveEvent();
+
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) {
             modal.style.display = "none";
             nameNode.removeChild(nameNode.firstChild);
             priceNode.removeChild(priceNode.firstChild);
             imageNode.removeChild(imageNode.firstChild);
             descriptionNode.removeChild(descriptionNode.firstChild);
             signUpCloseDateNode.removeChild(signUpCloseDateNode.firstChild);
-
-        }
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-                nameNode.removeChild(nameNode.firstChild);
-                priceNode.removeChild(priceNode.firstChild);
-                imageNode.removeChild(imageNode.firstChild);
-                descriptionNode.removeChild(descriptionNode.firstChild);
-                signUpCloseDateNode.removeChild(signUpCloseDateNode.firstChild);
-            }
         }
     }
+}
 
 function signUp() {
     var signUpSnackbar = document.getElementById("snackbar");
@@ -342,12 +331,9 @@ function moreInformationFunction(triggeringElement) {
         console.log("HELLO");
         if (window.location.pathname.indexOf("pastEvents") > -1) {
             displayPastEvents();
-            // call Past events method
         } else {
-            // call sign-ups method
             displaySignUps();
         }
-        // getEvents();
     });
 
 
