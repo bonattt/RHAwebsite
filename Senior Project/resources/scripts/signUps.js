c8bf26b890d5bf43117523a84af5b35d27d2798e
cookmn <cookmn@cookmn-w530g2.rose-hulman.edu>
2016-11-07 20:48:57 -0500

    Can now edit events!!!

--- Senior Project/resources/scripts/signUps.js
+++ Senior Project/resources/scripts/signUps.js
@@ -1,5 +1,23 @@
 
 var eventsMap = new Object();
+var eventId;
+var newEventName;
+var newEventPrice;
+var newEventImage;
+var newEventDescription;
+var newEventSignUpCloseDate;
+var nameInput = document.createElement("textarea");
+var priceInput = document.createElement("textarea");
+var descriptionInput = document.createElement("textarea");
+var signUpCloseDateInput = document.createElement("textarea");
+var imageInput = document.createElement("textarea");
+var nameNode = document.getElementById("nameInput");
+var priceNode = document.getElementById("priceInput");
+var imageNode = document.getElementById("imageInput");
+var descriptionNode = document.getElementById("descriptionInput");
+var signUpCloseDateNode = document.getElementById("signUpCloseDateInput");
+var editValue;
+
 function displayPastEvents() {
     var xhr = getEvents();
     xhr.send();
@@ -67,7 +85,7 @@ function displayPastEvents() {
 }
 
 function saveEvent() {
-    var url = apiURL + 'api/v1/events/1';
+    var url = apiURL + 'api/v1/events/' + eventId;
     function createCORSRequest(method, url) {
         var xhr = new XMLHttpRequest();
         console.log("xhr is: ");
@@ -90,7 +108,9 @@ function saveEvent() {
     xhr.onerror = function () {
         console.log("There was an error");
     }
-    xhr.send(JSON.stringify({ proposal_name: "It Works!!!" }));
+    console.log("new event name is: ");
+    console.log(newEventName);
+    xhr.send(JSON.stringify({ proposal_name: newEventName, cost_to_attendee: newEventPrice, image_path: newEventImage, description: newEventDescription, event_signup_close: newEventSignUpCloseDate }));
     return xhr;
 
 }
@@ -115,6 +135,7 @@ function displaySignUps() {
             var tileArea = document.getElementsByClassName("eventTileArea")[0];
             tileArea.innerHTML += html;
         }
+        console.log("events map is: ")
         console.log(eventsMap);
         var isAdmin = true;
         if (isAdmin) {
@@ -182,49 +203,40 @@ function displaySignUps() {
 
 }
 
-function showEditModal(editImage) {
-    console.log(editImage.srcElement);
+function showEditModal(edit) {
+    editValue = edit;
     var modal = document.getElementById('editModal');
     var span = document.getElementsByClassName("closeEdit")[0];
 
-    var title = editImage.srcElement.parentElement.innerHTML.split(" - ");
+    var title = editValue.srcElement.parentElement.innerHTML.split(" - ");
     var name = "Event name: ";
     var price = "Price: ";
     var image = "Image: ";
     var description = "Description: ";
     var signUpCloseDate = "Sign-up close date: ";
+    eventId = eventsMap[title[0]];
 
-    var nameInput = document.createElement("textarea");
     nameInput.setAttribute("rows", "1");
     nameInput.setAttribute("cols", "30");
     nameInput.innerHTML = title[0];
 
-    var priceInput = document.createElement("textarea");
     priceInput.setAttribute("rows", "1");
     priceInput.setAttribute("cols", "30");
     priceInput.innerHTML = title[1].split("<")[0];
 
-    var descriptionInput = document.createElement("textarea");
     descriptionInput.setAttribute("rows", "4");
     descriptionInput.setAttribute("cols", "30");
-    descriptionInput.innerHTML = editImage.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(6)")[0].innerHTML.split(" Sign-ups for this event will close on ")[0];
+    descriptionInput.innerHTML = editValue.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(6)")[0].innerHTML.split(" Sign-ups for this event will close on ")[0];
 
-    var signUpCloseDateInput = document.createElement("textarea");
     signUpCloseDateInput.setAttribute("rows", "1");
     signUpCloseDateInput.setAttribute("cols", "30");
-    signUpCloseDateInput.innerHTML = editImage.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(6)")[0].innerHTML.split(" Sign-ups for this event will close on ")[1].split(".")[0];
-    // console.log(editImage.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(2)")[0].currentSrc.split("images/")[1]);
+    signUpCloseDateInput.innerHTML = editValue.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(6)")[0].innerHTML.split(" Sign-ups for this event will close on ")[1].split(".")[0];
+    // console.log(editValue.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(2)")[0].currentSrc.split("images/")[1]);
 
-    var imageInput = document.createElement("textarea");
     imageInput.setAttribute("rows", "1");
     imageInput.setAttribute("cols", "30");
-    imageInput.innerHTML = editImage.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(2)")[0].currentSrc.split("images/")[1];
+    imageInput.innerHTML = editValue.srcElement.parentElement.parentElement.querySelectorAll(":nth-child(2)")[0].currentSrc.split("images/")[1];
 
-    var nameNode = document.getElementById("nameInput");
-    var priceNode = document.getElementById("priceInput");
-    var imageNode = document.getElementById("imageInput");
-    var descriptionNode = document.getElementById("descriptionInput");
-    var signUpCloseDateNode = document.getElementById("signUpCloseDateInput");
 
 
     document.getElementById("name").innerHTML = name;
@@ -239,6 +251,7 @@ function showEditModal(editImage) {
     signUpCloseDateNode.appendChild(signUpCloseDateInput);
 
 
+
     modal.style.display = "block";
     span.onclick = function () {
         modal.style.display = "none";
@@ -247,7 +260,6 @@ function showEditModal(editImage) {
         imageNode.removeChild(imageNode.firstChild);
         descriptionNode.removeChild(descriptionNode.firstChild);
         signUpCloseDateNode.removeChild(signUpCloseDateNode.firstChild);
-        saveEvent();
 
     }
     window.onclick = function (event) {
@@ -260,7 +272,14 @@ function showEditModal(editImage) {
             signUpCloseDateNode.removeChild(signUpCloseDateNode.firstChild);
         }
     }
+
+    var submitButton = document.getElementById("submit");
+    submitButton.addEventListener("click", submit);
+
 }
+
+
+
 
 function signUp() {
     var signUpSnackbar = document.getElementById("snackbar");
@@ -272,8 +291,6 @@ function moreInformationFunction(triggeringElement) {
     var parentDiv = triggeringElement.parentElement.parentElement;
     var linkClicked = parentDiv.getElementsByClassName("moreInfoLink")[0];
     var descriptionToShow = parentDiv.getElementsByClassName("moreInformation")[0];
-    console.log(linkClicked);
-    console.log(descriptionToShow);
 
     if (linkClicked.innerHTML == "Show Details") {
         descriptionToShow.style.display = "block";
@@ -338,3 +355,34 @@ function moreInformationFunction(triggeringElement) {
 
 
 })();
+
+function submit() {
+    var modal = document.getElementById('editModal');
+    console.log("hey there!");
+    modal.style.display = "none";
+    nameNode.removeChild(nameNode.firstChild);
+    priceNode.removeChild(priceNode.firstChild);
+    imageNode.removeChild(imageNode.firstChild);
+    descriptionNode.removeChild(descriptionNode.firstChild);
+    signUpCloseDateNode.removeChild(signUpCloseDateNode.firstChild);
+
+    newEventName = nameInput.value;
+    newEventPrice = priceInput.value;
+    newEventImage = imageInput.value;
+    newEventDescription = descriptionInput.value;
+    newEventSignUpCloseDate = signUpCloseDateInput.value;
+
+    var element = editValue.srcElement.parentElement;
+    element.parentElement.querySelectorAll(":nth-child(6)")[0].innerHTML = newEventDescription + " Sign-ups for this event will close on " + newEventSignUpCloseDate;
+    console.log(element.parentElement.querySelectorAll(":nth-child(6)")[0].innerHTML);
+    element.innerHTML = newEventName + " - " + newEventPrice;
+
+    var editPen = document.createElement("img");
+    editPen.setAttribute("src", "../images/edit.png");
+    element.appendChild(editPen);
+    editPen.addEventListener("click", function (e) {
+        showEditModal(e);
+    }, false);
+
+    saveEvent();
+}
