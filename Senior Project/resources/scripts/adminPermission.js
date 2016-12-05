0485339ceae1f28f24180e7521e5c184d874725e
bonattt <bonattt@rose-hulman.edu>
2016-12-05 16:30:19 -0500

    added generic attribute option to "insertEditButtons"

--- Senior Project/resources/scripts/adminPermission.js
+++ Senior Project/resources/scripts/adminPermission.js
@@ -10,14 +10,16 @@ var userIsOfficer = function(officers) {
 	return false;
 }
 
-var insertEditButtons = function(showModalFunc) {
+var insertEditButtons = function(showModalFunc, attributes) {
     var adminValues = document.getElementsByClassName("edit");
 	var buttonList = [];
     for (var i = 0; i < adminValues.length; i++) {
         var editButton = document.createElement("img");
         editButton.setAttribute("src", "../images/edit.png");
+		if (attributes != undefined) {
+			appendAttributes(editButton, attributes);
+		}
 		editButton.addEventListener("click", showModalFunc, false);
-		
 		adminValues[i].appendChild(editButton);
 		buttonList.push(editButton);
 	}
