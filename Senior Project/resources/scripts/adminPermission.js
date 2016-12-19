3ad36c65e458207d411bdb3a797dffd081fa14b0
cookmn <cookmn@cookmn-w530g2.rose-hulman.edu>
2016-12-19 17:03:27 -0500

    Showing true values in the modal

--- Senior Project/resources/scripts/adminPermission.js
+++ Senior Project/resources/scripts/adminPermission.js
@@ -1,5 +1,5 @@
 
-var userIsOfficer = function(officers) {
+var userIsOfficer = function (officers) {
 	officer = JSON.parse(officers);
     var tempUser = JSON.parse(sessionStorage.getItem("userData"));
 	if (!tempUser) {
@@ -13,7 +13,7 @@ var userIsOfficer = function(officers) {
 	return false;
 }
 
-var insertEditButtons = function(showModalFunc, dataElementId, targetIdRoot, attributes) {
+var insertEditButtons = function (showModalFunc, dataElementId, targetIdRoot, attributes) {
     var adminValues = document.getElementsByClassName("edit");
 	var buttonList = [];
     for (var i = 0; i < adminValues.length; i++) {
@@ -22,7 +22,7 @@ var insertEditButtons = function(showModalFunc, dataElementId, targetIdRoot, att
 		editButton.setAttribute("class", "admin-edit-button btn btn-info btn-lg");
 		editButton.setAttribute("data-toggle", "modal");	// data-toggle="modal"
 		editButton.setAttribute("data-target", "#myModal");	// data-target="#myModal"
-		editButton.setAttribute("onclick", "setupEditModal('"+dataElementId+"', '"+targetIdRoot+"');");
+		editButton.setAttribute("onclick", "setupEditModal('" + dataElementId + "', '" + targetIdRoot + "');");
 		if (attributes != undefined) {
 			appendAttributes(editButton, attributes);
 		}
@@ -32,7 +32,7 @@ var insertEditButtons = function(showModalFunc, dataElementId, targetIdRoot, att
 	}
 }
 
-var insertEditButtonsBefore = function(showModalFunc, attributes) {
+var insertEditButtonsBefore = function (showModalFunc, attributes) {
     var adminValues = document.getElementsByClassName("edit");
 	var buttonList = [];
     for (var i = 0; i < adminValues.length; i++) {
@@ -47,21 +47,21 @@ var insertEditButtonsBefore = function(showModalFunc, attributes) {
 	}
 }
 
-var appendAttributes = function(element, attributes) {
+var appendAttributes = function (element, attributes) {
 	for (attr in attributes) {
 		element.setAttribute(attr, attributes[attr]);
 	}
 }
 
-var setupEditModal = function(dataElementId, targetIdRoot) {
+var setupEditModal = function (dataElementId, targetIdRoot) {
 	console.log("adminPermission.SETUP EDIT MODAL");
 	var dataset = document.getElementById(dataElementId).dataset;
 	for (attr in dataset) {
 		var textField = document.getElementById(targetIdRoot + attr);
-		if (textField == undefined) {continue;}
+		if (textField == undefined) { continue; }
 		textField.value = dataset[attr];
 	}
-	
+
 	/*var nameField = document.getElementById(targetIdRoot + "name");
 	nameField.value = dataset.name;
 	
@@ -69,9 +69,9 @@ var setupEditModal = function(dataElementId, targetIdRoot) {
 	descriptionField.value = dataset.desc; //*/
 }
 
-var enableSubmitButton = function(dataElementId, targetIdRoot) {
+var enableSubmitButton = function (dataElementId, targetIdRoot) {
 	var submitButton = document.getElementById("modal-submit");
-	submitButton.addEventListener("click", function(event) {
+	submitButton.addEventListener("click", function (event) {
 		var msg = "TODO: add a database query here! \n";
 		var dataset = document.getElementById(dataElementId).dataset;
 		for (attr in dataset) {
