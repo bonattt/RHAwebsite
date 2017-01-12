39efb1b5c763e18fa367b78dac8dbce0492e6eab
seanmmcpherson <mcphersm@rose-hulman.edu>
2017-01-12 17:02:26 -0500

    modal displays
    also have a decent idea how Im planning to switch between calendars

--- Senior Project/resources/scripts/equipmentReservation.js
+++ Senior Project/resources/scripts/equipmentReservation.js
@@ -1,15 +1,21 @@
 function setup() {
 	var addButton = document.getElementById("calendar-add-button");
-	// addButton.addEventListener("click", function() {addNewCalendar() });
+	addButton.addEventListener("click", function() {addNewCalendar("calendar1", "calendar-modal-")});
+
+	var testButton = document.getElementById("test-calendar-button");
+	var dataset = document.getElementById("calendar2").dataset;
+	testButton.addEventListener("click", function() {switchCalendarView(dataset)});
 }
 
-var addNewCalendar = function (dataElementId, targetIdRoot) {
-	console.log("adminPermission.SETUP EDIT MODAL");
+ function addNewCalendar(dataElementId, targetIdRoot) {
+	console.log("whatup tho");
 	var dataset = document.getElementById(dataElementId).dataset;
 	for (attr in dataset) {
 		var textField = document.getElementById(targetIdRoot + attr);
 		if (textField == undefined) { continue; }
 		textField.value = dataset[attr];
+
+		console.log(textField);
 	}
 
 	/*var nameField = document.getElementById(targetIdRoot + "name");
@@ -18,6 +24,17 @@ var addNewCalendar = function (dataElementId, targetIdRoot) {
 	var descriptionField = document.getElementById(targetIdRoot + "desc");
 	descriptionField.value = dataset.desc; //*/
 }
+
+function switchCalendarView(calendarToView) {
+	console.log("I'm happening!");
+	var calendarFrame = document.getElementById("calendar-wrapper");
+	console.log(calendarFrame);
+	console.log(calendarToView);
+	console.log(calendarToView["embed"]);
+
+	calendarFrame.innerHTML = calendarToView["embed"];
+
+};
 
 $(document).ready(function() {
 	setup();
