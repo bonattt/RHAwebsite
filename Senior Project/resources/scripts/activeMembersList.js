6624e8deb7eae0948dd7e2c8c19173f01744240d
cookmn <cookmn@cookmn-w530g2.rose-hulman.edu>
2017-01-16 17:33:54 -0500

    worked on displaying all members page

--- Senior Project/resources/scripts/activeMembersList.js
+++ Senior Project/resources/scripts/activeMembersList.js
@@ -1,32 +1,53 @@
 function setup() {
-	var urlExtension = 'funds/';
-	var xhr = xhrGetRequest(urlExtension);
+    var urlExtension = 'funds/';
+    var xhr = xhrGetRequest(urlExtension);
     xhr.send();
-    setTimeout(function() {createHTMLFromResponseText(xhr.responseText)}, 300);
+    setTimeout(function () { createHTMLFromResponseText(xhr.responseText) }, 300);
 
     function createHTMLFromResponseText(funds) {
         funds = JSON.parse(funds);
-        var html = "<table border='1' align='center' bordercolor='#808080' id='floor-money'><tbody><tr>";
-        html += "<td align='middle' width='200'><b>Floor</b></td><td align='middle' width='60'><b>Balance</b></td>";
-        html += "<td align='middle' width='200'><b>Floor</b></td><td align='middle' width='60'><b>Balance</b></td></tr>";
+        var floorMoneyTable = document.getElementById("activeMembersTable");
+        var html = "<table border='1' align='center' bordercolor='#808080' id='membersTable'><tbody><tr>";
+        html += "<td align='middle' width='200'><b>Name</b></td><td align='middle' width='200'><b>Hall</b></td><td align='middle' width='50'><b>Active</b></td>";
         var countForColoring = 0;
-        for (var i = 0; i < funds.length - 1; i += 2) {
+        for (var i = 0; i < funds.length - 1; i += 1) {
+            console.log(i);
             if (funds[i].display_on_site) {
-                if(countForColoring % 2 == 0){
-                html += "<tr bgcolor='#f0f0f0'><td>" + funds[i].fund_name + "</td>";
-                } else{
-                html += "<tr><td>" + funds[i].fund_name + "</td>";
+                if (countForColoring % 2 == 0) {
+                    html += "<tr bgcolor='#f0f0f0' id='memberID" + i + "'><td>" + funds[i].fund_name + "</td>";
+                } else {
+                    html += "<tr id='memberID" + i + "'><td>" + funds[i].fund_name + "</td>";
                 }
                 html += "<td align='right'>" + funds[i].funds_amount + "</td>";
-                html += "<td>" + funds[i + 1].fund_name + "</td>";
-                html += "<td align='right'>" + funds[i + 1].funds_amount + "</td></tr>";
+                html += "<td align='right'>" + funds[i].funds_amount + "</td></tr>";
                 countForColoring++;
             }
+            //floorMoneyTable.innerHTML += html;
+            //console.log(html);
+            //html = "";
+            //var tr = document.getElementById("memberID" + i);
+            //tr.addEventListener("click", showModal);
         }
-        var floorMoneyTable = document.getElementById("activeMembersTable");
         floorMoneyTable.innerHTML += html;
+        for (var i = 0; i < funds.length - 1; i += 1) {
+            var getBy = "memberID" + i;
+            //console.log(getBy);
+            //var tr = document.getElementById(getBy);
+            //console.log(tr);
+            //tr.addEventListener("click", showModal);
+            var table = document.getElementById("membersTable");
+            var rows = table.getElementsByTagName("tr");
+            for (i = 0; i < rows.length; i++) {
+                row = table.rows[i];
+                console.log(row.innerHTML);
+            }
+        }
     }
-} 
-document.addEventListener("DOMContentLoaded", function(event) {
+}
+document.addEventListener("DOMContentLoaded", function (event) {
     setup();
 });
+
+function showModal() {
+    alert("showing modal, in theory.");
+}
