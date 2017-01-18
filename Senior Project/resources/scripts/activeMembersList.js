8149b88f0e92db331ff9541b6a7b92cee44fb764
cookmn <cookmn@cookmn-w530g2.rose-hulman.edu>
2017-01-18 15:59:27 -0500

    Finished members page
    You can toggle between active and all

--- Senior Project/resources/scripts/activeMembersList.js
+++ Senior Project/resources/scripts/activeMembersList.js
@@ -1,3 +1,6 @@
+
+var displayingAllMembers = true;
+
 function setup() {
     var urlExtension = 'members/';
     var xhr = xhrGetRequest(urlExtension);
@@ -5,11 +8,14 @@ function setup() {
     setTimeout(function () { createHTMLFromResponseText(xhr.responseText) }, 300);
 
     function createHTMLFromResponseText(members) {
-        if (document.getElementById('allMembers').checked) {
-            alert("show all members!");
-        } else if (document.getElementById('activeMembers').checked) {
-            alert("show active members!");
-        }
+        drawAllMembersTable(members);
+        var allMembersButton = document.getElementById('allMembers');
+        $(".allMembers").change(function () {
+            displayOtherTable(members);
+        });
+        $(".activeMembers").change(function () {
+            displayOtherTable(members);
+        });
     }
 }
 document.addEventListener("DOMContentLoaded", function (event) {
@@ -23,11 +29,11 @@ function showModal() {
 function drawAllMembersTable(members) {
     members = JSON.parse(members);
     var floorMoneyTable = document.getElementById("activeMembersTable");
+    floorMoneyTable.innerHTML = "";
     var html = "<table border='1' align='center' bordercolor='#808080' id='membersTable'><tbody><tr>";
     html += "<td align='middle' width='200'><b>Name</b></td><td align='middle' width='200'><b>Hall</b></td><td align='middle' width='50'><b>Active</b></td>";
     var countForColoring = 0;
     for (var i = 0; i < members.length - 1; i += 1) {
-        console.log(i);
         if (countForColoring % 2 == 0) {
             html += "<tr bgcolor='#f0f0f0' id='memberType" + i + "' data-toggle='modal' data-target='#myModal'><td>" + members[i].firstname + " " + members[i].lastname + "</td>";
         } else {
@@ -57,4 +63,57 @@ function drawAllMembersTable(members) {
         //row.addEventListener("click", showModal);
     }
     //}
+}
+
+function drawActiveMembersTable(members) {
+    members = JSON.parse(members);
+    var floorMoneyTable = document.getElementById("activeMembersTable");
+    floorMoneyTable.innerHTML = "";
+    var html = "<table border='1' align='center' bordercolor='#808080' id='membersTable'><tbody><tr>";
+    html += "<td align='middle' width='200'><b>Name</b></td><td align='middle' width='200'><b>Hall</b></td><td align='middle' width='50'><b>Active</b></td>";
+    var countForColoring = 0;
+    for (var i = 0; i < members.length - 1; i += 1) {
+        if (members[i].active) {
+            if (countForColoring % 2 == 0) {
+                html += "<tr bgcolor='#f0f0f0' id='memberType" + i + "' data-toggle='modal' data-target='#myModal'><td>" + members[i].firstname + " " + members[i].lastname + "</td>";
+            } else {
+                html += "<tr id='memberType" + i + "'><td>" + members[i].firstname + " " + members[i].lastname + "</td>";
+            }
+            html += "<td>" + members[i].hall + "</td>";
+            html += "<td>" + members[i].active + "</td></tr>";
+            countForColoring++;
+        }
+        //floorMoneyTable.innerHTML += html;
+        //console.log(html);
+        //html = "";
+        //var tr = document.getElementById("memberID" + i);
+        //tr.addEventListener("click", showModal);
+    }
+    floorMoneyTable.innerHTML += html;
+    //for (var i = 0; i < funds.length - 1; i += 1) {
+    // var getBy = i;
+    // console.log(getBy);
+    // var tr = floorMoneyTable.getElementById(getBy);
+    // console.log(tr);
+    // tr.addEventListener("click", showModal);
+    var table = document.getElementById("membersTable");
+    var rows = table.getElementsByTagName("tr");
+    for (i = 0; i < rows.length; i++) {
+        row = table.rows[i];
+
+        //row.addEventListener("click", showModal);
+    }
+    //}
+}
+
+function displayOtherTable(members) {
+    if(displayingAllMembers){
+        drawActiveMembersTable(members);
+        displayingAllMembers = false;
+        console.log("displaying active");
+    } else{
+        drawAllMembersTable(members);
+        displayingAllMembers = true;
+        console.log("displaying all");
+    }
 }
