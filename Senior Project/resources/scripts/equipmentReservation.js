bf1bd1146c39a850104f18957bb72cd994b8ecb9
seanmmcpherson <mcphersm@rose-hulman.edu>
2017-01-18 17:25:32 -0500

    can select calendars
    yay

--- Senior Project/resources/scripts/equipmentReservation.js
+++ Senior Project/resources/scripts/equipmentReservation.js
@@ -1,10 +1,19 @@
 function setup() {
-	var addButton = document.getElementById("calendar-add-button");
-	addButton.addEventListener("click", function() {addNewCalendar("calendar1", "calendar-modal-")});
+	// var addButton = document.getElementById("calendar-add-button");
+	// addButton.addEventListener("click", function() {addNewCalendar("calendar1", "calendar-modal-")});
 
-	var testButton = document.getElementById("test-calendar-button");
+	var select = document.getElementById("calendar-selector");
+	select.onchange = function() {
+		var selIndex = select.selectedIndex + 1;
+		console.log(selIndex);
+		var selValue = $('select option:nth-child(' + selIndex + ')').val();
+		console.log(selValue);
+		var calendarToSelect = document.getElementById(selValue);
+		console.log(calendarToSelect);
+		switchCalendarView(calendarToSelect);
+	}
+
 	var dataset = document.getElementById("calendar2").dataset;
-	testButton.addEventListener("click", function() {switchCalendarView(dataset)});
 }
 
  function addNewCalendar(dataElementId, targetIdRoot) {
@@ -28,11 +37,15 @@ function setup() {
 function switchCalendarView(calendarToView) {
 	console.log("I'm happening!");
 	var calendarFrame = document.getElementById("calendar-wrapper");
+	if (calendarToView == null) {
+		calendarFrame.innerHTML = "";
+		return;
+	}
 	console.log(calendarFrame);
 	console.log(calendarToView);
-	console.log(calendarToView["embed"]);
+	console.log(calendarToView.getAttribute("data-embed"));
 
-	calendarFrame.innerHTML = calendarToView["embed"];
+	calendarFrame.innerHTML = calendarToView.getAttribute("data-embed");
 
 };
 
