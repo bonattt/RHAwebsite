3b49bbe42be305b40e4ac1d320d2da69640e198a
cookmn <cookmn@cookmn-w530g2.rose-hulman.edu>
2017-01-19 15:32:13 -0500

    Working on the pop up modal with officer information

--- Senior Project/resources/scripts/activeMembersList.js
+++ Senior Project/resources/scripts/activeMembersList.js
@@ -31,34 +31,29 @@ function drawAllMembersTable(members) {
     var floorMoneyTable = document.getElementById("activeMembersTable");
     floorMoneyTable.innerHTML = "";
     var html = "<table border='1' align='center' bordercolor='#808080' id='membersTable'><tbody><tr>";
-    html += "<td align='middle' width='200'><b>Name</b></td><td align='middle' width='200'><b>Hall</b></td><td align='middle' width='50'><b>Active</b></td>";
+    html += "<td align='middle' width='200'><b>Name</b></td><td align='middle' width='200'><b>Hall</b></td>";
     var countForColoring = 0;
     for (var i = 0; i < members.length - 1; i += 1) {
         if (countForColoring % 2 == 0) {
-            html += "<tr bgcolor='#f0f0f0' id='memberType" + i + "' data-toggle='modal' data-target='#myModal'><td>" + members[i].firstname + " " + members[i].lastname + "</td>";
+            html += "<tr bgcolor='#f0f0f0' id='member" + i + "' data-toggle='modal' data-target='#myModal'><td>" + members[i].firstname + " " + members[i].lastname + "</td>";
         } else {
-            html += "<tr id='memberType" + i + "' data-toggle='modal' data-target='#myModal'><td>" + members[i].firstname + " " + members[i].lastname + "</td>";
+            html += "<tr id='member" + i + "' data-toggle='modal' data-target='#myModal'><td>" + members[i].firstname + " " + members[i].lastname + "</td>";
         }
-        html += "<td>" + members[i].hall + "</td>";
-        html += "<td>" + members[i].active + "</td></tr>";
+        html += "<td>" + members[i].hall + "</td></tr>";
         countForColoring++;
-        //floorMoneyTable.innerHTML += html;
-        //console.log(html);
-        //html = "";
-        //var tr = document.getElementById("memberID" + i);
-        //tr.addEventListener("click", showModal);
     }
     floorMoneyTable.innerHTML += html;
-    //for (var i = 0; i < funds.length - 1; i += 1) {
-    // var getBy = i;
-    // console.log(getBy);
-    // var tr = floorMoneyTable.getElementById(getBy);
-    // console.log(tr);
-    // tr.addEventListener("click", showModal);
     var table = document.getElementById("membersTable");
     var rows = table.getElementsByTagName("tr");
-    for (i = 0; i < rows.length; i++) {
+    for (i = 0; i < members.length - 1; i++) {
         row = table.rows[i];
+        var dataset = document.getElementById('member' + i).dataset;
+        var fields = ["firstname", "lastname", "username", "membertype", "phone_number", "hall", "room_number", "cm", "active"]
+        fields.forEach(function (field) {
+            console.log("setting field " + field + " to " + members[i][field]);
+            dataset[field] = members[i][field];
+        });
+        document.getElementById("member-modal-name").innerHTML = members[i].firstname;
 
         //row.addEventListener("click", showModal);
     }
@@ -70,7 +65,7 @@ function drawActiveMembersTable(members) {
     var floorMoneyTable = document.getElementById("activeMembersTable");
     floorMoneyTable.innerHTML = "";
     var html = "<table border='1' align='center' bordercolor='#808080' id='membersTable'><tbody><tr>";
-    html += "<td align='middle' width='200'><b>Name</b></td><td align='middle' width='200'><b>Hall</b></td><td align='middle' width='50'><b>Active</b></td>";
+    html += "<td align='middle' width='200'><b>Name</b></td><td align='middle' width='200'><b>Hall</b></td>";
     var countForColoring = 0;
     for (var i = 0; i < members.length - 1; i += 1) {
         if (members[i].active) {
@@ -79,8 +74,7 @@ function drawActiveMembersTable(members) {
             } else {
                 html += "<tr id='memberType" + i + "' data-toggle='modal' data-target='#myModal'><td>" + members[i].firstname + " " + members[i].lastname + "</td>";
             }
-            html += "<td>" + members[i].hall + "</td>";
-            html += "<td>" + members[i].active + "</td></tr>";
+            html += "<td>" + members[i].hall + "</td></tr>";
             countForColoring++;
         }
         //floorMoneyTable.innerHTML += html;
@@ -107,11 +101,11 @@ function drawActiveMembersTable(members) {
 }
 
 function displayOtherTable(members) {
-    if(displayingAllMembers){
+    if (displayingAllMembers) {
         drawActiveMembersTable(members);
         displayingAllMembers = false;
         console.log("displaying active");
-    } else{
+    } else {
         drawAllMembersTable(members);
         displayingAllMembers = true;
         console.log("displaying all");
