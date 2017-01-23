27fc7bc89c74df1c09fa6e5ae832f4a9a593f7c8
cookmn <cookmn@cookmn-w530g2.rose-hulman.edu>
2017-01-23 16:10:47 -0500

    deleted unnecessary code

--- Senior Project/resources/scripts/floorMoney.js
+++ Senior Project/resources/scripts/floorMoney.js
@@ -50,32 +50,6 @@ function setup() {
         }
         table.appendChild(tbdy);
         body.appendChild(table);
-
-
-
-
-
-
-
-        // var html = "<table border='1' align='center' bordercolor='#808080' id='floor-money'><tbody><tr>";
-        // html += "<td align='middle' width='200'><b>Floor</b></td><td align='middle' width='60'><b>Balance</b></td>";
-        // html += "<td align='middle' width='200'><b>Floor</b></td><td align='middle' width='60'><b>Balance</b></td></tr>";
-        // var countForColoring = 0;
-        // for (var i = 0; i < funds.length - 1; i += 2) {
-        //     if (funds[i].display_on_site) {
-        //         if (countForColoring % 2 == 0) {
-        //             html += "<tr bgcolor='#f0f0f0'><td>" + funds[i].fund_name + "</td>";
-        //         } else {
-        //             html += "<tr><td>" + funds[i].fund_name + "</td>";
-        //         }
-        //         html += "<td align='right'>" + funds[i].funds_amount + "</td>";
-        //         html += "<td>" + funds[i + 1].fund_name + "</td>";
-        //         html += "<td align='right'>" + funds[i + 1].funds_amount + "</td></tr>";
-        //         countForColoring++;
-        //     }
-        // }
-        // var floorMoneyTable = document.getElementById("floorMoneyTable");
-        // floorMoneyTable.innerHTML += html;
     }
 }
 document.addEventListener("DOMContentLoaded", function (event) {
