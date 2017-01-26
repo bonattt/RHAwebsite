df55887cba52893a6eeae6eede410b57bda90f99
cookmn <cookmn@cookmn-w530g2.rose-hulman.edu>
2017-01-26 16:48:49 -0500

    Still working on displaying the correct images :(

--- Senior Project/resources/scripts/photoGallery.js
+++ Senior Project/resources/scripts/photoGallery.js
@@ -3,12 +3,33 @@
 //Have a modal pop up that has that image on it
 
 function setup() {
-    var images = document.getElementsByClassName("photoGalleryImage");
-    console.log("in setup");
-    for (var i = 0; i < images.length; i++) {
-        images[i].addEventListener('click', function () { showPictureModal("../images/events/tri-hop.png"); });
-        console.log("in for loop");
-    }
+    // var images = document.getElementsByClassName("photoGalleryImage");
+    // console.log("in setup");
+    // for (var i = 0; i < images.length; i++) {
+    //     images[i].addEventListener('click', function () { showPictureModal("../images/events/tri-hop.png"); });
+    //     console.log("in for loop");
+    // }
+    var fileExt = {};
+    fileExt[0] = ".png";
+    fileExt[1] = ".jpg";
+    fileExt[2] = ".gif";
+    $.ajax({
+        //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+        url: 'resources/images/gallery',
+        success: function (data) {
+            console.log("data is: ");
+            // $("#fileNames").html('<ul>');
+            // //List all png or jpg or gif file names in the page
+            // // console.log(data);
+            // $(data).find("a:contains(" + fileExt[0] + "),a:contains(" + fileExt[1] + "),a:contains(" + fileExt[2] + ")").each(function () {
+            //     console.log("found an image");
+            //     var filename = this.href.replace(window.location.host, "").replace("http:///", "");
+            //     $("#fileNames").append("<li>" + filename + "</li>");
+            // });
+            // $("#fileNames").append('</ul>');
+        }
+    });
+    // document.getElementById("fileNames").innerHTML = "<img class='photoGalleryImage' src='../images/gallery/31da25d45be0dbb169ee52557995c2e6_PRAISE-HELIX.png'>";
 }
 
 function showPictureModal(source) {
@@ -31,7 +52,7 @@ function showPictureModal(source) {
 }
 
 function uploadPhoto() {
-    var photoUploadApi = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port: '') + '/api/v1/galleryPhoto';
+    var photoUploadApi = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + '/api/v1/galleryPhoto';
     var photoxhr = new XMLHttpRequest();
     var files = document.getElementById("imageFile").files;
 
@@ -40,7 +61,7 @@ function uploadPhoto() {
     photoxhr.open('POST', photoUploadApi, true);
 
     photoxhr.onreadystatechange = function (e) {
-        if(photoxhr.readyState == 4 && photoxhr.status == 200) {
+        if (photoxhr.readyState == 4 && photoxhr.status == 200) {
             $('#uploadModal').modal('hide');
         }
     };
