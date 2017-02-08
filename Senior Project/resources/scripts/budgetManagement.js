077b32e59b376f9f1c424de3341df541d1139d58
bonattt <bonattt@rose-hulman.edu>
2017-02-08 16:21:01 -0500

    made a table to display payments

--- Senior Project/resources/scripts/budgetManagement.js
+++ Senior Project/resources/scripts/budgetManagement.js
@@ -1,4 +1,5 @@
-
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"]
 
 function parseModalEntries(idHeader, ids) {
     var json_obj = {};
@@ -16,6 +17,11 @@ function clearModalEntries(idHeader, ids) {
     });
 }
 
+function prepEventName(str) {
+    str = str.toLowerCase();
+    return str.replace(/ /g, '');
+}
+
 function setupButtons() {
     var addPaymentButton = document.getElementById("addPaymentButton");
     addPaymentButton.setAttribute('data-toggle', 'modal');
@@ -23,15 +29,30 @@ function setupButtons() {
 
     var addPaymentSubmit = document.getElementById('paymentModal-submit');
     addPaymentSubmit.addEventListener('click', function() {
-        var entryIds = ['funds_amount', 'username', 'event', 'cm', 'turnin_date', 'processed_date'];
+        var entryIds = [
+                'amountUsed', 'accountCode', 'receiver', 'description',
+                'dateProcessed', 'dateReceived'
+        ];
         var modalId = 'paymentModal-';
         var json_obj = parseModalEntries(modalId, entryIds);
+
+        json_obj.dateProcessed = new Date(json_obj.dateProcessed);
+        json_obj.dateReceived = new Date(json_obj.dateReceived);
+        var select = document.getElementById('paymentModal-event');
+        console.log(select.value);
+        console.log(select.dataset);
+        json_obj.proposal_id = select.dataset[prepEventName(select.value)]
+        json_obj.reciepts = {"test1": "hello", "test2": "world!"};
+
         var apiUrl = '???';
         var xhr = xhrPostRequest(apiUrl);
         xhr.onload = function() {alert('successfully sent!');}
-        // xhr.send(JSON.stringify(json_obj));
-        clearModalEntries(modalId, entryIds);
+        xhr.send(JSON.stringify(json_obj));
+        alert(JSON.stringify(json_obj));
+        //clearModalEntries(modalId, entryIds);
     });
+
+    return;
 
     var addChargeButton = document.getElementById("addChargeButton");
     addChargeButton.setAttribute('data-toggle', 'modal');
@@ -66,8 +87,79 @@ function setupButtons() {
     });
 }
 
+function populatePaymentsTable() {
+    var xhr = xhrGetRequest('payments/');
+    var tbody = document.getElementById('paymentsTable');
+    xhr.onload = function() {
+        var payments = JSON.parse(xhr.responseText);
+        payments.forEach(function(entry) {
+            var row = buildPaymentColumn(entry);
+            tbody.appendChild(row);
+        });
+    }
+    xhr.send();
+}
+
+function buildPaymentColumn(payment) {
+    var row = document.createElement('tr');
+
+//    var col = document.createElement('td');
+//    col.appendChild(document.createTextNode(payment.expenses_id));
+//    row.appendChild(col);
+
+    col = document.createElement('td');
+    col.appendChild(document.createTextNode(payment.proposal_id));
+    row.appendChild(col);
+
+    col = document.createElement('td');
+    col.appendChild(document.createTextNode(payment.cm));
+    row.appendChild(col);
+
+    col = document.createElement('td');
+    col.appendChild(document.createTextNode(payment.receiver));
+    row.appendChild(col);
+
+    col = document.createElement('td');
+    col.appendChild(document.createTextNode(payment.amountused));
+    row.appendChild(col);
+
+//    col = document.createElement('td');
+//    col.appendChild(document.createTextNode(payment.description));
+//    row.appendChild(col);
+//
+//    col = document.createElement('td');
+//    col.appendChild(document.createTextNode(payment.accountcode));
+//    row.appendChild(col);
+
+    col = document.createElement('td');
+    col.appendChild(document.createTextNode(composeDateStr(payment.datereceived)));
+    row.appendChild(col);
+
+    col = document.createElement('td');
+    col.appendChild(document.createTextNode(composeDateStr(payment.dateprocessed)));
+    row.appendChild(col);
+
+    col = document.createElement('td');
+    col.appendChild(document.createTextNode(payment.reciepts));
+    row.appendChild(col);
+
+    return row;
+}
+
+function composeDateStr(dateStr) {
+    var msg = '';
+    var date = new Date(dateStr);
+    msg += monthNames[date.getMonth()]
+    msg += ' ';
+    msg += date.getDate();
+    msg += ', ';
+    msg += date.getFullYear();
+    return msg;
+}
+
 function setAdmin() {
     setupButtons();
+    populatePaymentsTable();
 }
 
 function setup() {
