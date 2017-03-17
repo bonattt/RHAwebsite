9e3ab8127c75e89ac9d3ce13619d279cddbb2c85
seanmmcpherson <mcphersm@rose-hulman.edu>
2017-03-17 09:56:46 -0400

    increased delay
    now reliably waits until all the data has been pushed

--- Senior Project/resources/scripts/activeMembersList.js
+++ Senior Project/resources/scripts/activeMembersList.js
@@ -287,7 +287,7 @@ function setupSubmitAttendanceButton() {
 
                 xhr.onreadystatechange = function (e) {
                     if (xhr.readyState == 4 && xhr.status == 200) {
-                        setTimeout(function () { window.reload()}, 400);
+                        setTimeout(function () { location.reload()}, 600);
                     }
                 };
                 xhr.send(JSON.stringify({ membersToUpdate: result }));
