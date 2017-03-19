23dd73dce7f095ed182f13bb73124900fb39d4bc
bonattt <bonattt@rose-hulman.edu>
2017-03-19 12:49:55 -0400

    commented out unneeded rewire.js tests that break
    these tests were only testing my configuration of the test tool
    rewire.js, which I have now determined is not a tool I need or should be
    using, so I removed the tests rather than fixing them.

--- test/sampleTest.js
+++ test/sampleTest.js
@@ -9,14 +9,14 @@ describe("Sample Example", function() {
         });
     });
 
-    describe("using rewire", function() {
-        it("return one", function() {
-            var module = rewire("../server.js");
-            expect(1).to.equal(parseInt("1"));
-        });
-        it("say hello", function(){
-            var module = rewire("../test/seeIfRewireJsWorks.js");
-            expect("hello world").to.equal(module.sayHello());
-        });
-    });
+//    describe("using rewire", function() {
+//        it("return one", function() {
+//            var module = rewire("../server.js");
+//            expect(1).to.equal(parseInt("1"));
+//        });
+//        it("say hello", function(){
+//            var module = rewire("../test/seeIfRewireJsWorks.js");
+//            expect("hello world").to.equal(module.sayHello());
+//        });
+//    });
 });
