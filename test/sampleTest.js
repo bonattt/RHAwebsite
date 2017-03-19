847b1fccbd32570ce05ee05043d172b30b15459d
bonattt <bonattt@rose-hulman.edu>
2017-03-19 11:54:19 -0400

    added rewire.js for testing
    As it turned out, this will not be able to do what I was trying to get
    it to do, but it may be useful later for testing other things.

--- test/sampleTest.js
+++ test/sampleTest.js
@@ -1,11 +1,22 @@
 var expect = require("chai").expect;
-
+var rewire = require("rewire");
 
 describe("Sample Example", function() {
     describe("sample quality", function() {
         it("does a thing", function() {
             expect("hello").to.equal("hello");
             expect([1, 2, 3]).to.deep.equal([1, 2, 3]);
+        });
+    });
+
+    describe("using rewire", function() {
+        it("return one", function() {
+            var module = rewire("../server.js");
+            expect(1).to.equal(parseInt("1"));
+        });
+        it("say hello", function(){
+            var module = rewire("../test/seeIfRewireJsWorks.js");
+            expect("hello world").to.equal(module.sayHello());
         });
     });
 });
