b1ae4110634ea40d4606c9c963238e86a1a4cb91
bonattt <bonattt@rose-hulman.edu>
2017-03-20 15:08:18 -0400

    wrote some tests for Budget Management, found a bug
    buildRow(...) is not increasing the column count as it builds a row,
    resulting in redundant ID vals

--- resources/tests/testBudgetManagement.js
+++ resources/tests/testBudgetManagement.js
@@ -1,8 +1,25 @@
+$.holdReady(true);
 
 window.onload = function() {
-	QUnit.test( "", function( assert ) {
-	  //showModal();
-	  assert.ok( 1 == "1", "Passed!" );
+	QUnit.test( "Build Row", function( assert ) {
+        //showModal();
+        var mochData = {"field1": "data1", "field2": "data2", "field3": 3, "field4": 4};
+        var rowNumber = 3;
+        var fields = ["field1", "field3"];
+
+        var row = buildRow(mochData, fields, 3);
+
+        assert.equal(row.getAttribute("id"), "row3", "has correct id");
+        // appends blank <td/> tag
+        assert.equal(row.children.length, fields.length+1,  "only makes columns for selected fields");
+
+        assert.ok(!row.children[0].getAttribute("id"), "first entry has no id");
+        assert.ok(!row.children[0].getAttribute("class"), "first entry has no class");
+        assert.equal(row.children[0].children.length, 0, "first entry has no children");
+
+        assert.equal(row.children[1].getAttribute("id"), "row"+rowNumber+"col1", "column 1 has correct id");
+        assert.equal(row.children[2].getAttribute("id"), "row"+rowNumber+"col2", "column 2 has correct id");
+
 	});
 
 }
