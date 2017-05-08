8d9940c372366924dc0ed188a65ac863099b6ed2
cookmn <cookmn@cookmn-w530g2.rose-hulman.edu>
2017-05-08 15:23:03 -0400

    added a null check for phone number

--- resources/scripts/officers.js
+++ resources/scripts/officers.js
@@ -215,7 +215,9 @@ function setup() {
                 html += "</h3>";
                 html += "<img src='" + officer[i].image + "' alt='" + officer[i].membertype + "'height='294' width='195'>";
                 html += "<p>Email: <a href='mailto:" + officer[i].username + "@rose-hulman.edu'>" + officer[i].username + "@rose-hulman.edu</a></p>";
-                html += "<p> Phone Number: " + parsePhoneNumber(officer[i].phone_number) + "</p>";
+                if (officer[i].phone_number) {
+                    html += "<p> Phone Number: " + parsePhoneNumber(officer[i].phone_number) + "</p>";
+                }
                 html += "<p> Room: " + officer[i].hall + " " + officer[i].room_number + "</p>";
                 html += "<p>Box #: " + officer[i].cm + "</p>";
 
